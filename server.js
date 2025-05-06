8b12e0b2a17e644e01e7e1a143bc6bb8b9c14657
zccarballo <zchacar.genius@gmail.com>
2025-05-06 13:59:59 +0200

    rama-zoe actualizada

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+// Accedo o arquivo estático
+app.use(express.static(path.join(__dirname, "public")));
+
+// GETTERS
+// <a href="/ivan">enlace</a>
+app.get("/ivan",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/ivan.html"))
+})
+// <a href="/password">enlace</a>
+app.get("/password",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/password.html"))
+})
+// <a href="/index">enlace</a>
+app.get("/index",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/index.html"))
+})
+// <a href="/teachers">enlace</a>
+app.get("/teachers",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/marilo.teachers.html"))
+})
+
+// <a href="/students">enlace</a>
+app.get("/students",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/marilo.students.html"))
+})
+
+// <a href="/menu">enlace</a>
+app.get("/menu",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/index-zoemarilo.html"))
+})
+
+
+// <a href="/dashboard">enlace</a>
+app.get("/dashboard",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/zoe.index.html"))
+    /* res.send({
+        mensaxe:"estou na de zoe"
+    }) */
+})
+
+
+
+//START SERVER
+app.listen(3000, function () {
+ console.log("Server running");
+});
