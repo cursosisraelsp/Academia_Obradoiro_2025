bd587dc7fa00ce61a62bcf04efdb9ac8bc2cf06b
AndreFernandezGenius <aferigl.genius@gmail.com>
2025-05-06 13:02:39 +0200

    cambio andresito

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ app.get("/index",(req,res)=>{
 app.get("/teachers",(req,res)=>{
     res.sendFile(path.join(__dirname,"./public/views/teachers.html"))
 })
-
+// <a href="/andre">enlace</a>
+app.get("/andre",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/views/andre.html"))
+})
 
 
 //START SERVER
